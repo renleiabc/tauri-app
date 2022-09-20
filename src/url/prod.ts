20a0008750da65707ebd3a7676cc2d44a448005e
renlei <421796896@qq.com>
2022-09-20 15:55:30 +0800

    mac

--- src/url/prod.ts
+++ src/url/prod.ts
@@ -1,7 +1,7 @@
 export default {
-  nodeserver: 'http://192.168.1.191:7079',
+  nodeserver: 'https://testnet-sg-34-87-136-38.ibax.io:8079',
   '//notes 2': 'back-up services',
-  walletserver: 'http://192.168.1.191:7023',
+  walletserver: 'https://testnet-jp-35-187-216-251.ibax.io:7023',
   '//notes 5': 'browser address',
-  blockexplorer: 'http://scan.ibax.io'
+  blockexplorer: 'https://testscan.ibax.network'
 };
